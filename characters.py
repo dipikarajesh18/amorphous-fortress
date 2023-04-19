7df5051f42375b59ba0ff3061bbf34d29e3320dd
dipikarajesh18 <dipika.rajesh@gmail.com>
2023-04-19 17:43:57 -0400

    add character class to create graph

---
+++ characters.py
@@ -0,0 +1,26 @@
+import yaml
+import random
+
+with open('alpha_config.yaml', 'r') as file:
+    config = yaml.safe_load(file)
+
+class CharacterClass:
+    def __init__(self, character):
+        self.character = character
+        self.actions = []
+        self.selectActions()
+        self.printClassDetails()
+    
+    def printClassDetails(self):
+        print(self.character, self.actions)
+    
+    def selectActions(self):
+        self.actions = random.sample(config['action_space'], random.randint(1, len(config['action_space'])))
+
+if __name__ == "__main__":
+    characterList = random.sample(config['character'], random.randint(1, len(config['character'])))
+    for character in characterList:
+        CharacterClass(character)
+    
+    
+    
